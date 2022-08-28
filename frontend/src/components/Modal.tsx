d8c647b1bf5be85d69f695b8c420a34cac851eaa
小林大斗 <khiroto@kobayashidaitonoMacBook-Pro.local>
2022-08-28 14:23:04 +0900

    選手追加ページを作成

---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import MUIModal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  minWidth: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: 1,
+};
+
+type Props = {
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const Modal: React.FC<Props> = ({ children,isOpen, onClose }) => {
+  return (
+    <div>
+      <MUIModal
+        open={isOpen}
+        onClose={onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>{children}</Box>
+      </MUIModal>
+    </div>
+  );
+};
+
+export default Modal;
---
+++ frontend/src/features/player/types/index.ts
@@ -0,0 +1,6 @@
+export type Player = {
+  name: string;
+  memo: string;
+  number: number | null;
+  position: number | null;
+}
